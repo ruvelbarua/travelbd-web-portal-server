5c65ee59c569f4a484b4e40076b5e9b9c601b061
ruvelbarua <ruvel.br78@gmail.com>
2021-10-31 19:00:00 +0600

    update

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 
+const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ziv9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://travelbdpro:YtcKgR64o6ZGfYkB@cluster0.ziv9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
     try {
-        await client.connect()
-        console.log('database connected successfully')
-    }
-    finally {
+        await client.connect();
+        const database = client.db("travelbd");
+        const usersCollection = database.collection("users");
+
+        // GET API
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+        // POST API
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const result = await usersCollection.insertOne(newUser);
+            // console.log('Get new user', req.body);
+            // console.log('add user', result);
+            res.json(result);
+        })
+
+    } finally {
         // await client.close();
     }
 }
@@ -27,7 +43,6 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('Travel BD Service is Running');
 });
-
 
 app.listen(port, () => {
     console.log('Server Running at Port', port)
