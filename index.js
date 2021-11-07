f80e1bb0b661313e3251775ab687929539ba3e4a
ruvelbarua <ruvel.br78@gmail.com>
2021-11-07 18:13:48 +0600

    update

--- index.js
+++ index.js
@@ -19,23 +19,20 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const database = client.db("travels");
-        const serviceCollection = database.collection("services");
+        const serviceCollection = client.db("travels").collection("services")
+
+        const bookingsCollection = client.db("travels").collection("bookings")
+        // // const database = client.db("travels");
+        // // const serviceCollection = database.collection("services");
+
+        // const database = client.db("travels");
+        // const bookingsCollection = database.collection("bookings");
 
         // GET SERVICES API
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find({});
             const services = await cursor.toArray();
             res.send(services);
-        });
-
-        // GET SINGLE SERVICE
-        app.get('/services/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log('Getting Id', id);
-            const query = { _id: objectId(id) };
-            const service = await serviceCollection.findOne(query);
-            res.json(service);
         });
 
         // POST API
@@ -51,7 +48,39 @@ async function run() {
             const query = { _id: objectId(id) };
             const result = await serviceCollection.deleteOne(query);
             res.json(result);
-        })
+        });
+
+        // GET SINGLE SERVICE
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('Getting Id', id);
+            const query = { _id: objectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.json(service);
+        });
+
+        // POST BOOKING DATA
+        app.post("/myBooking", async (req, res) => {
+            const order = req.body;
+            const result = await bookingsCollection.insertOne(order);
+            res.send(result);
+        });
+
+        // GET BOOKING DATA
+        app.get("/myOrders/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log('Getting Id', id);
+            const query = { _id: objectId(id) };
+            const service = await bookingsCollection.findOne(query);
+            res.json(service);
+
+            // const result = await bookingsCollection
+            //     .find({ email: req.params.email })
+            //     .toArray();
+            // res.send(result);
+        });
+
+
 
     } finally {
         // await client.close();
@@ -62,6 +91,8 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('Travel BD Server Running');
 });
+
+// app.listen(process.env.PORT || port)
 
 app.listen(port, () => {
     console.log('Travel BD Server on Port', port)
